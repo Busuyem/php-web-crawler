5f54cf7437c7a024951a510d369048e70a7e4e06
busuyem <owoyemibusuyi@gmail.com>
2023-12-07 09:56:12 +0100

    add colour property to data array

--- src/Scrape.php
+++ src/Scrape.php
@@ -15,17 +15,16 @@ class Scrape
         $document = ScrapeHelper::fetchDocument('https://www.magpiehq.com/developer-challenge/smartphones');
 
             $nodeValues = $document->filter('.product')->each(function (Crawler $node, $i){
-            $title = $node->text();
+            //$title = $node->text();
             $title = $node->filter('h3')->text();
             $price = $node->filter('.text-sm')->previousAll()->text();
             $image = $node->filter('img')->attr('src');
             $capacity = $node->filter('.product-capacity')->text();
-            $colour = $node->filter('.bg-white')->last()->text();
+            $colour = $node->filter('.flex-wrap > .px-2 > span')->attr('data-colour');
             $availabilityText = str_replace('Availability:', '', $node->filter('.text-sm')->previousAll()->nextAll()->text());
             $isAvailable = str_contains($node->filter('.text-sm')->previousAll()->nextAll()->text(), 'In Stock') ? "true" : "false";
             $shippingText = $node->filter('.bg-white > div')->last()->text();
             $shippingDate = $node->filter('.bg-white')->text();
-            
 
             $data = [
                 'title' => $title,
@@ -39,9 +38,12 @@ class Scrape
 
             ];
 
-            print_r(json_encode($data, 200));
+            //return $data;
+            print_r($data);
             //echo $title;
         });
+
+        //print_r(json_encode($nodeValues, 200));
 
         file_put_contents('output.json', json_encode($this->products));
     }
